260222e5dd36777d308b61dfc0320dc9be54e3de
Jim Paine <jim@jimpaine.io>
2018-10-12 15:43:54 +0100

    switched to inline

--- scripts/transform-proxies.sh
+++ scripts/transform-proxies.sh
@@ -1,3 +1,3 @@
-sudo blob_path=$(terraform output -json primary_blob_endpoint | jq '.value' | tr -d '"')   
-sudo container="proxy"   
-sudo sed -i "s|{bloblocation}|$blob_path$container|g" ../src/ImageProcessor/proxies.json   
+blob_path=$(terraform output -json primary_blob_endpoint | jq '.value' | tr -d '"')   
+container="proxy"   
+sed -i "s|{bloblocation}|$blob_path$container|g" ../src/ImageProcessor/proxies.json   
