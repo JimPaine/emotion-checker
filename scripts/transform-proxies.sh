acf8c11642d352cb3ce5989f0561505ace31f2b2
Jim Paine <jim@jimpaine.io>
2018-10-12 15:51:25 +0100

    tidied up after switching to bash

--- scripts/transform-proxies.sh
+++
@@ -1,3 +0,0 @@
-blob_path=$(terraform output -json primary_blob_endpoint | jq '.value' | tr -d '"')   
-container="proxy"   
-sed -i "s|{bloblocation}|$blob_path$container|g" ../src/ImageProcessor/proxies.json   
