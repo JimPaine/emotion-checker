bf0736da2524d2d01c37d6f6b0fd2a4ca258fd97
Jim Paine <jim@jimpaine.io>
2019-03-13 07:56:54 +0000

    added missing providers

--- env/providers.tf
+++ env/providers.tf
@@ -9,3 +9,13 @@ provider "azurerm" {
 provider "random" {
   version = "~> 1.3"
 }
+
+provider "acme" {
+  server_url = "https://acme-v02.api.letsencrypt.org/directory"
+  version = "~>1.1.1"
+}
+
+provider "dnsimple" {
+  token = "${var.dnsimple_auth_token}"
+  account = "${var.dnsimple_account}"
+}
