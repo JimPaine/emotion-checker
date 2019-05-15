d51c1f2df2309f839b12caa3c1bfec12d556e234
Jim Paine <jim@jimpaine.io>
2019-05-15 16:44:37 +0100

    Changed logging dep

--- src/ImageProcessor/Startup.cs
+++ src/ImageProcessor/Startup.cs
@@ -1,6 +1,7 @@
 using System.Net.Http;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 [assembly: FunctionsStartup(typeof(ImageProcessor.Startup))]
 
@@ -9,8 +10,9 @@ namespace ImageProcessor
     public class Startup : FunctionsStartup
     {
         public override void Configure(IFunctionsHostBuilder builder)
-        {            
-            builder.Services.AddLogging();
+        {           
+            builder.Services.AddTransient<LoggerFactory>();
+            builder.Services.AddSingleton<ILogger<Function>, Logger<Function>>();
             builder.Services.AddSingleton<HttpClient>(new HttpClient());
         }
     }
